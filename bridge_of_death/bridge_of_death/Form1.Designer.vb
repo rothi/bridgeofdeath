f379cecb641ca42729f91220d755b4b8bbf7d0df
rothi <rothi@pagosa.k12.co.us>
2014-09-19 09:54:51 -0600

    Added Project Files

---
+++ bridge_of_death/bridge_of_death/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBridge
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAnswer = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtQuest = New System.Windows.Forms.TextBox()
+        Me.txtColor = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.btnRestart = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnAnswer
+        '
+        Me.btnAnswer.Location = New System.Drawing.Point(222, 177)
+        Me.btnAnswer.Name = "btnAnswer"
+        Me.btnAnswer.Size = New System.Drawing.Size(75, 23)
+        Me.btnAnswer.TabIndex = 1
+        Me.btnAnswer.Text = "Answer"
+        Me.btnAnswer.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(322, 85)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 3
+        '
+        'txtQuest
+        '
+        Me.txtQuest.Location = New System.Drawing.Point(322, 114)
+        Me.txtQuest.Name = "txtQuest"
+        Me.txtQuest.Size = New System.Drawing.Size(100, 20)
+        Me.txtQuest.TabIndex = 4
+        '
+        'txtColor
+        '
+        Me.txtColor.Location = New System.Drawing.Point(322, 144)
+        Me.txtColor.Name = "txtColor"
+        Me.txtColor.Size = New System.Drawing.Size(100, 20)
+        Me.txtColor.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.Label1.Location = New System.Drawing.Point(73, 82)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(224, 24)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Stop! What is your name?"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.Label2.Location = New System.Drawing.Point(63, 140)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(234, 24)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "What is your favorite color?"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.Label3.Location = New System.Drawing.Point(123, 110)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(174, 24)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "What is your quest?"
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer.Location = New System.Drawing.Point(63, 238)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(0, 24)
+        Me.lblAnswer.TabIndex = 9
+        '
+        'btnRestart
+        '
+        Me.btnRestart.Location = New System.Drawing.Point(547, 255)
+        Me.btnRestart.Name = "btnRestart"
+        Me.btnRestart.Size = New System.Drawing.Size(75, 23)
+        Me.btnRestart.TabIndex = 10
+        Me.btnRestart.Text = "Restart"
+        Me.btnRestart.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(71, 38)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(211, 31)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Answer In Order"
+        '
+        'frmBridge
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(634, 290)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnRestart)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtColor)
+        Me.Controls.Add(Me.txtQuest)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnAnswer)
+        Me.Name = "frmBridge"
+        Me.Text = "Bridge of Death"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnAnswer As System.Windows.Forms.Button
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents txtQuest As System.Windows.Forms.TextBox
+    Friend WithEvents txtColor As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+    Friend WithEvents btnRestart As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ bridge_of_death/bridge_of_death/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class frmBridge
+
+    Private Sub btnAnswer_Click(sender As Object, e As EventArgs) Handles btnAnswer.Click
+
+        Dim strName As String = "Ian Roth"
+        Dim strGuessName As String = Me.txtName.Text
+
+        Dim strQuest As String = "To Seek the Holy Grail"
+        Dim strGuessQuest As String = Me.txtQuest.Text
+
+        Dim strColor As String = "Blue"
+        Dim strGuessColor As String = Me.txtColor.Text
+
+        If strGuessName <> "Ian Roth" Then
+            Me.lblAnswer.Text = "Wrong Name. You Die"
+
+        ElseIf strGuessQuest <> "To Seek the Holy Grail" Then
+            Me.lblAnswer.Text = "That Isn't Your Quest. You Die"
+
+        ElseIf strGuessColor <> "Blue" Then
+            Me.lblAnswer.Text = "That Isn't Your Favorite Color. You Die"
+
+        ElseIf strGuessColor = "Blue" Then
+            Me.lblAnswer.Text = "You May Pass"
+
+        End If
+    End Sub
+
+    Private Sub btnRestart_Click(sender As Object, e As EventArgs) Handles btnRestart.Click
+        Me.lblAnswer.Text = ""
+        Me.txtName.Text = ""
+        Me.txtQuest.Text = ""
+        Me.txtColor.Text = ""
+    End Sub
+End Class
